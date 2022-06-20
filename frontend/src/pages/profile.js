d515b8bbf9b879b6a42df98a35fe5ef9f0ae3e45
Mis311 <55878506+Mis311@users.noreply.github.com>
2022-06-21 00:27:39 +0900

    Update profile.js

--- frontend/src/pages/profile.js
+++ frontend/src/pages/profile.js
@@ -4,7 +4,11 @@ export default function Profile() {
       className="column min-screen-height"
       style={{ marginLeft: "6%", paddingTop: "5rem" }}
     >
-      <h1>Profile</h1>
+      <h1>Your Progress</h1>
+      <h1>NFT Certificate</h1>
+      <h1>Your Partners</h1>
+      <h1>Marketplace<Coming Soon...></h1>
+
     </div>
   )
 }
