2fcc34f74df65b759a6aef0368d3ebee5d8bc2e8
AcmeGamers <52010365+AcmeGamers@users.noreply.github.com>
2022-06-21 03:57:12 +0500

    Adding progress bar

--- frontend/src/pages/profile.js
+++ frontend/src/pages/profile.js
@@ -1,7 +1,26 @@
+function BoxArea(props) {
+  return (
+    <div className="course-data row hr-center">
+      <img
+        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.seoclerk.com%2Fpics%2F500304-1KuyMq1482510124.jpg&f=1&nofb=1"
+        alt="HTML"
+        className="course-image"
+      />
+
+      <p className="row hr-center" style={{ marginLeft: "20px" }}>
+        <span>Progress</span>
+        {/* progress bar */}
+        <span className="progress-bar"></span>
+        <span>100%</span>
+      </p>
+    </div>
+  );
+}
+
 export default function Profile(props) {
   return (
     <div className="min-screen-height row user-profile">
-      <div style={{ width: "40%" }}>
+      <div style={{ width: "20%" }} className="column hr-center">
         <img
           src={
             props.authData ??
@@ -10,16 +29,23 @@ export default function Profile(props) {
           alt={props.authData ?? ""}
           className="user-image"
         />
-        <h1>{props.authData ?? "Kaide"}</h1>
+        <p style={{ margin: "10px" }}>{props.authData ?? "Kaide"}</p>
         <p>{props.authData ?? "data@gmail.com"}</p>
       </div>
-      <div style={{ width: "60%" }}>
-        {/* <h1>Your Progress</h1>
-      <h1>NFT Certificate</h1>
-      <h1>Your Partners</h1>
-      <h1>Questions:</h1>
-      <p>Which course/media/article taught you the most about React.js?</p>
-      <h1>Marketplace</h1> */}
+      <div style={{ width: "80%", padding: "0 5%" }}>
+        <div>
+          <h1>Your Progress</h1>
+          <BoxArea />
+        </div>
+        <div>
+          <h1>Certificates</h1>
+        </div>
+        <div>
+          <h1>Your Partners</h1>
+        </div>
+        <div>
+          <h1>Marketplace</h1>
+        </div>
       </div>
     </div>
   );
