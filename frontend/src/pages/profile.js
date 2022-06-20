330198486ff9aa931effc184d1b8c5a0285e9704
Mis311 <55878506+Mis311@users.noreply.github.com>
2022-06-21 00:31:08 +0900

    Update profile.js

--- frontend/src/pages/profile.js
+++ frontend/src/pages/profile.js
@@ -7,6 +7,8 @@ export default function Profile() {
       <h1>Your Progress</h1>
       <h1>NFT Certificate</h1>
       <h1>Your Partners</h1>
+      <h1>Questions:</h1>
+        <p>Which course/media/article taught you the most about React.js?</p>
       <h1>Marketplace<Coming Soon...></h1>
 
     </div>
